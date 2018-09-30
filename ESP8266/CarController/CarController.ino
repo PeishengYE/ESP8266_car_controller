26edba6ea64f7f287515f8b258ccd5853c7ecab4
Peisheng YE <peisheng.ye.88@gmail.com>
2018-09-30 12:46:52 -0400

    ESP.deepSleep with reboot after sleep time expired, sending realtime ADC value

--- ESP8266/CarController/CarController.ino
+++ ESP8266/CarController/CarController.ino
@@ -14,15 +14,26 @@
 */
 
 #include <Ticker.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+
 #define STATUS_LED_PIN 4
 #define ADC_LIMIT  550
-
+ESP8266WebServer server(80);
 Ticker status_led_ticker;
 Ticker light_strength_ticker;
 //ESP8266WebServer server(80);
+/* Set these to your desired credentials. */
+const char *ssid = "YEP_ESP";
+const char *password = "yep123";
 
 
 int status_led_working = 0;
+void handleRoot() {
+        int adc_value = analogRead(A0);
+	server.send(200, "text/html", "<h1>You are connected to YEP WIFI: with ADC: " + String(adc_value) + "</h1>");
+}
 
 void flip() {
   int state = digitalRead(STATUS_LED_PIN);  // get the current state of GPIO1 pin
@@ -47,16 +58,22 @@ void checking_ADC() {
 
  if (adc_value >= ADC_LIMIT){
      status_led_working = 0;
+	 server.begin();
+	 Serial.println("HTTP server started");
+
 
  }else{
 
      status_led_working = 1;
+	 server.stop();
+	 Serial.println("HTTP server stopped>> deepsleep for 5s with WAKE_RF_DEFAULT");
+	 ESP.deepSleep(5000000, WAKE_RF_DEFAULT);
  }
 }
 
 void setup() {
 	Serial.begin(115200);
-    Serial.println("");
+    Serial.println("setup()>> ");
 
   pinMode(STATUS_LED_PIN, OUTPUT);
   pinMode(A0, INPUT);
@@ -66,7 +83,18 @@ void setup() {
   // flip the pin every 0.3s
   status_led_ticker.attach(0.3, flip);
   light_strength_ticker.attach(0.4, checking_ADC);
+      Serial.print("Configuring access point...");
+	/* You can remove the password parameter if you want the AP to be open. */
+	WiFi.softAP(ssid, password);
+
+	IPAddress myIP = WiFi.softAPIP(); 
+	Serial.print("AP IP address: ");
+	Serial.println(myIP);
+	server.on("/", handleRoot);
+      Serial.println("setup()<< ");
+
 }
 
 void loop() {
+	 server.handleClient();
 }
